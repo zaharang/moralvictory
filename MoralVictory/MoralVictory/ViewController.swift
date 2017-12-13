f8af7ecc174d22e54c19d03282613ab715f6dcc2
신관호 <khshin@nemustech.com>
2017-12-13 20:23:08 +0900

    init

---
+++ MoralVictory/MoralVictory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoralVictory
+//
+//  Created by 김도윤 on 2017. 12. 13..
+//  Copyright © 2017년 김도윤. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
