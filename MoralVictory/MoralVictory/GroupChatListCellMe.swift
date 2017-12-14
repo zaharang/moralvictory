57e0a6004031336a8c691ee858bf310b79032c9a
신관호 <khshin@nemustech.com>
2017-12-14 12:01:16 +0900

    add constarint

--- MoralVictory/MoralVictory/GroupChatListCellMe.swift
+++ MoralVictory/MoralVictory/GroupChatListCellMe.swift
@@ -12,20 +12,26 @@ class GroupChatListCellMe: GroupChatListCell {
 
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        talkLabelGroup.autoPinEdge(.left, to: .left, of: contentView)
+        talkLabelGroup.autoPinEdge(.right, to: .right, of: contentView, withOffset: 5)
+        talkLabelGroup.autoPinEdge(.top, to: .top, of: contentView)
+        talkLabelGroup.autoPinEdge(.bottom, to: .bottom, of: contentView)
+        
+        talkLabelBaloonGroup.autoPinEdge(.left, to: .left, of: talkLabelGroup)
+        talkLabelBaloonGroup.autoPinEdge(.right, to: .right, of: talkLabelGroup)
+        talkLabelBaloonGroup.autoPinEdge(.top, to: .top, of: talkLabelGroup)
+        talkLabelBaloonGroup.autoPinEdge(.bottom, to: .bottom, of: talkLabelGroup)
 
-        profileImageView.autoPinEdge(.left, to: .left, of: contentView, withOffset: 20)
-        profileImageView.autoSetDimension(.width, toSize: imageViewSize)
-        profileImageView.autoSetDimension(.height, toSize: imageViewSize)
-        profileImageView.autoAlignAxis(toSuperviewAxis: .horizontal)
-
-        profileNameLabel.autoPinEdge(.left, to: .right, of: profileImageView, withOffset: 20)
-        profileNameLabel.autoPinEdge(.right, to: .right, of: contentView)
-        profileNameLabel.autoPinEdge(.top, to: .top, of: contentView)
-
-        talkLabel.autoPinEdge(.left, to: .right, of: profileImageView, withOffset: 20)
-        talkLabel.autoPinEdge(.right, to: .right, of: contentView)
-        talkLabel.autoPinEdge(.top, to: .bottom, of: profileNameLabel)
-        talkLabel.autoPinEdge(.bottom, to: .bottom, of: contentView)
+        talkLabel.autoPinEdge(.left, to: .left, of: talkLabelBaloonGroup, withOffset: 10)
+        talkLabel.autoPinEdge(.right, to: .right, of: talkLabelBaloonGroup, withOffset: -10)
+        talkLabel.autoPinEdge(.top, to: .top, of: talkLabelBaloonGroup)
+        talkLabel.autoPinEdge(.bottom, to: .bottom, of: talkLabelBaloonGroup)
+        
+        talkLabelImageView.autoPinEdge(.left, to: .left, of: talkLabelBaloonGroup)
+        talkLabelImageView.autoPinEdge(.right, to: .right, of: talkLabelBaloonGroup)
+        talkLabelImageView.autoPinEdge(.top, to: .top, of: talkLabelBaloonGroup)
+        talkLabelImageView.autoPinEdge(.bottom, to: .bottom, of: talkLabelBaloonGroup)
     }
 
     required init?(coder aDecoder: NSCoder) {
